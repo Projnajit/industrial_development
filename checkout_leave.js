948478dbbbb7ad1c0f6e5ce1b81e2f7e3f0ad4f3
projnajit <projnajit@gmail.com>
2023-04-11 08:05:25 +0600

    assignment-2 commited

---
+++ checkout_leave.js
@@ -0,0 +1,97 @@
+let ss=sessionStorage.getItem('dataArray');
+let data=JSON.parse(ss);
+let table=document.getElementById("table1");
+console.log(data);
+
+///PUSHING DATA TO THE TABLE
+for(let i=0;i<data.length;i++)
+{
+    let row=table.insertRow(i+1);
+    for(let j=0;j<6;j++)
+    {
+        let cell=row.insertCell(j);
+        if(j==4) cell.innerHTML=`<td><img id="img" src=${data[i][j]} alt=""></td>`
+        else if(j==5) cell.innerHTML=`<td><a href="index.html" class="accept">Edit</a><input type='button' value="Reject" class="reject"/></td>`
+        else cell.innerHTML=data[i][j];
+    }
+}
+///
+
+
+
+///SEARCHING AND SHOWING THE DATA
+function searching()
+{
+    let filter=document.getElementById("khojo").value.toUpperCase();
+    let tr=table.getElementsByTagName("tr");
+    for(let i=0;i<tr.length;i++)
+    {
+        let tableCell=tr[i].getElementsByTagName("td")[0];
+        if(tableCell)
+        {
+            let text=tableCell.textContent || tableCell.innerHTML;
+            if(text.toUpperCase().indexOf(filter)>-1) tr[i].style.display="";
+            else tr[i].style.display="none";
+        }
+        
+    }
+}
+//
+
+
+
+
+///REMOVING ROW 
+function deleterow(evnt)
+{
+    if(!evnt.target.classList.contains("reject"))  return;
+    let rowNumber=evnt.target.closest("tr").rowIndex;
+    console.log(rowNumber);
+    evnt.target.closest("tr").remove();
+    data.splice(rowNumber-1,1);
+    console.log(data);
+    return;
+}
+table.addEventListener("click",deleterow);
+///
+
+
+
+
+///RESPOND TO CLICK TABLE DATA
+function mainClick()
+{
+    //console.log(event.target.parentElement.rowIndex);
+}
+//
+
+
+
+
+///PASSING DATA-ARRAY TO INDEX-PAGE
+function transfer(ev)
+{
+    if(!ev.target.classList.contains("accept"))  return;
+
+    let row=ev.target.closest("tr").rowIndex-1; //getting row id
+    
+    sessionStorage.setItem('array',JSON.stringify(data));
+    sessionStorage.setItem('rowIdx',JSON.stringify(row));
+    window.location.href="index.html";
+
+    return;
+}
+table.addEventListener("click",transfer);
+///
+
+
+
+function backToHome(e)
+{
+    if(!e.target.classList.contains("home"))  return;
+    sessionStorage.setItem('passing', JSON.stringify(data));
+    window.location.href('index.html');
+}
+
+
+document.querySelector(".home").addEventListener("click",backToHome);
---
+++ index.js
@@ -0,0 +1,75 @@
+let data=[];
+let x=document.forms["form1"];
+let flag=false;
+
+
+
+///CONCATENATION THE tempDATA-ARRAY WITH DATA-ARRAY
+let ss=sessionStorage.getItem('array');
+let ss2=sessionStorage.getItem('passing');
+let index=sessionStorage.getItem('rowIdx');
+let tempData=JSON.parse(ss); ///CATCHING BY EDIT BUTTON
+let tempData2=JSON.parse(ss2); ///CATHING BY HOME BUTTON
+let tempIndex=JSON.parse(index);
+
+if(tempData!=null) data=tempData;
+if(tempData2!=null) data=tempData2;
+
+
+if(tempIndex!=null)
+{
+    for(let i=0;i<x.length-1;i++)
+    {
+        x.elements[i].value=data[tempIndex][i];
+    }
+}
+
+
+///PUSHING INPUTS TO DATA-ARRAY
+function submitCall()
+{
+    let object={},flag=true;
+    for(let i=0;i<x.length;i++)
+    {   
+        object[i]=(x.elements[i].value);
+        if(flag===true && object[i]==="")
+        {
+            alert("Fill all the given Input correctly!");
+            flag=false;
+        }
+        if(i===2 && flag===true)
+        {
+            if(object[i]>7 || object[i]<1)
+            {
+                alert("Make leave-day range in between (1-7) days!");
+                flag=false;
+            }
+        }
+        x.elements[i].value="";
+    }
+    if(flag) 
+    {
+        if(tempIndex!=null) 
+        {
+            console.log("cheeers!!!");
+            data[tempIndex]=object;
+            tempIndex=null;
+        }
+        else data.push(object);
+        console.log(data);
+    }
+}
+
+
+
+
+///PASSING DATA-ARRAY to NEWPAGE
+document.getElementById("newpage").onclick=function()
+{
+    sessionStorage.setItem('dataArray',JSON.stringify(data));
+    window.location.href="checkout_leave.html";
+}
+///
+
+
+
