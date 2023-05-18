cfc08da72c5c5fd9be1ac24b46740c9a54eb7c6b
projnajit <projnajit@gmail.com>
2023-05-18 15:18:00 +0600

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 
 function print()
 {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Hi, I am react app</p>
+    </>
+  )
 }
 export default print;
